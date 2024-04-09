87e1f11e81bf3d118616bf44e0f2f635277ffc59
Edward Tanguay <edward@tanguay.info>
2024-04-09 14:42:59 +0200

    base

---
+++ src/routes/contacts/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik"
+
+export default component$(() => {
+    return (
+        <div>
+            <h1 class="text-5xl">Contacts</h1>
+            <p class="mt-6 flex item-center justify-center">This is the contacts page.</p>
+        </div>
+    )
+})
